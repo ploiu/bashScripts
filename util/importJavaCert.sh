d1ba048c2fa72cb6148219533f21d0ca4a53b3e5
ploiu <ploiu415@gmail.com>
2024-01-03 20:28:07 -0500

    update importJavaCert

--- util/importJavaCert.sh
+++ util/importJavaCert.sh
@@ -3,17 +3,23 @@ function importJavaCert {
 		echo "$(purpleFg importJavaCert)"
 		echo -e "\tImports a cert from your system keystore into the java keystore"
 		echo "Usage:"
-		echo -e "\t$(purpleFg importJavaCert) $(cyanFg '<certname>') $(cyanFg '<alias>')"
+		echo -e "\t$(purpleFg importJavaCert) $(cyanFg '<certname>') $(cyanFg '<alias>') $(cyanFg '[keytoolLocation]')"
 		echo "Notes:"
 		echo -e "\tThis will call sudo"
 	}
 
 local certName="$1"
 local alias="$2"
+local keyToolLocation="$3"
 [[ "$certName" = "" ]] && showHelp && return;
 [[ "$alias" = "" ]] && showHelp && return;
 [[ -e "/usr/local/share/ca-certificates/$certName.crt" ]] || echo "/usr/local/share/ca-certificates/$certName.crt does not exist!" || return 1;
 
-sudo keytool -importcert -alias $alias -file /usr/local/share/ca-certificates/$certName.crt -cacerts -trustcacerts
+
+if [ "$keyToolLocation" = "" ]; then
+	sudo keytool -importcert -alias $alias -file /usr/local/share/ca-certificates/$certName.crt -cacerts -trustcacerts
+else
+	sudo $keyToolLocation -importcert -alias $alias -file /usr/local/share/ca-certificates/$certName.crt -cacerts -trustcacerts
+fi
 
 }
