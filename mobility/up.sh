cc5f75a9e28076434c3a9e19e89ec4581ee226eb
ploiu <ploiu415@gmail.com>
2025-11-01 09:18:07 -0400

    fix up not setting back properly

--- mobility/up.sh
+++ mobility/up.sh
@@ -1,9 +1,9 @@
 function up {
   local origDir="$(pwd)"
+    # register with back
+  export backDir="$origDir"
   local count=$1
   # if count wasn't specified, default to 1
   [[ "$count" = "" ]] && cd ../ && return
   for _ in $(seq 1 $count); do cd ../; done
-  # register with back
-  export backDir="$origDir"
 }
